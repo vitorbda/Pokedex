df23d9b714b66d6d0317a88bd32594842ca16f3e
João Vitor de Andrade <joao.andrade@grxsolucoes.com.br>
2023-03-20 23:40:54 -0300

    Finalização da tela de detalhes

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -14,6 +14,7 @@ export class PokeListComponent implements OnInit{
   constructor(
     private pokeApiService: PokeApiService
   ){}
+  
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe(
       res => {
