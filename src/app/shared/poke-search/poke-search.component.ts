dcafddce7245a3d13b3d3e0ef75160e8d15ebdef
João Vitor de Andrade <joao.andrade@grxsolucoes.com.br>
2023-03-20 22:49:41 -0300

    Criação da função de busca

--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'poke-search',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class PokeSearchComponent {
 
+  @Output() public emmitSearch: EventEmitter<string> = new EventEmitter();
+
+  constructor(){}
+
+  public search(value: string){
+    console.log(value);
+  }
 }
